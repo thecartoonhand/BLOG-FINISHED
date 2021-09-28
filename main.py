ba18d548438b70cc54824f5f92d8e3c6616e6990
Alexander Persson <alexander.s.persson@gmail.com>
2021-09-28 12:18:03 +0200

    add psycopg2-binary

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
