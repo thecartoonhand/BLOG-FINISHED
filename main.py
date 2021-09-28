24425caef89911d491bee48b0ae0197800e65719
Alexander Persson <alexander.s.persson@gmail.com>
2021-09-28 12:43:53 +0200

    removed author field from edit route

--- main.py
+++ main.py
@@ -201,7 +201,6 @@ def edit_post(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
